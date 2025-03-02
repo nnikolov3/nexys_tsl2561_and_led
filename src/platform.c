cc09b91353b9d7afaff6e8b4b304486e83a10bb5
Niko Nikolov <nikolay.niko.nikolov@gmail.com>
2025-03-01 18:31:59 -0800

    Lux values working + refactor

--- src/platform.c
+++ src/platform.c
@@ -30,9 +30,10 @@
  *
  ******************************************************************************/
 
-#include "platform_config.h"
 #include "xil_cache.h"
 #include "xparameters.h"
+
+#include "platform_config.h"
 
 /*
  * Uncomment one of the following two lines, depending on the target,
@@ -48,59 +49,58 @@
 #define UART_BAUD 9600
 #endif
 
-void enable_caches( )
+void enable_caches ( )
 {
 #ifdef __PPC__
-        Xil_ICacheEnableRegion( CACHEABLE_REGION_MASK );
-        Xil_DCacheEnableRegion( CACHEABLE_REGION_MASK );
+    Xil_ICacheEnableRegion ( CACHEABLE_REGION_MASK );
+    Xil_DCacheEnableRegion ( CACHEABLE_REGION_MASK );
 #elif __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-        Xil_ICacheEnable( );
+    Xil_ICacheEnable ( );
 #endif
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-        Xil_DCacheEnable( );
+    Xil_DCacheEnable ( );
 #endif
 #endif
 }
 
-void disable_caches( )
+void disable_caches ( )
 {
 #ifdef __MICROBLAZE__
 #ifdef XPAR_MICROBLAZE_USE_DCACHE
-        Xil_DCacheDisable( );
+    Xil_DCacheDisable ( );
 #endif
 #ifdef XPAR_MICROBLAZE_USE_ICACHE
-        Xil_ICacheDisable( );
+    Xil_ICacheDisable ( );
 #endif
 #endif
 }
 
-void init_uart( )
+void init_uart ( )
 {
 #ifdef STDOUT_IS_16550
-        XUartNs550_SetBaud(
-          STDOUT_BASEADDR, XPAR_XUARTNS550_CLOCK_HZ, UART_BAUD );
-        XUartNs550_SetLineControlReg( STDOUT_BASEADDR, XUN_LCR_8_DATA_BITS );
+    XUartNs550_SetBaud ( STDOUT_BASEADDR, XPAR_XUARTNS550_CLOCK_HZ, UART_BAUD );
+    XUartNs550_SetLineControlReg ( STDOUT_BASEADDR, XUN_LCR_8_DATA_BITS );
 #endif
-        /* Bootrom/BSP configures PS7/PSU UART to 115200 bps */
+    /* Bootrom/BSP configures PS7/PSU UART to 115200 bps */
 }
 
-void init_platform( )
+void init_platform ( )
 {
-        /*
-         * If you want to run this example outside of SDK,
-         * uncomment one of the following two lines and also #include
-         * "ps7_init.h" or #include "ps7_init.h" at the top, depending on the
-         * target. Make sure that the ps7/psu_init.c and ps7/psu_init.h files
-         * are included along with this example source files for compilation.
-         */
-        /* ps7_init();*/
-        /* psu_init();*/
-        enable_caches( );
-        init_uart( );
+    /*
+     * If you want to run this example outside of SDK,
+     * uncomment one of the following two lines and also #include "ps7_init.h"
+     * or #include "ps7_init.h" at the top, depending on the target.
+     * Make sure that the ps7/psu_init.c and ps7/psu_init.h files are included
+     * along with this example source files for compilation.
+     */
+    /* ps7_init();*/
+    /* psu_init();*/
+    enable_caches ( );
+    init_uart ( );
 }
 
-void cleanup_platform( )
+void cleanup_platform ( )
 {
-        disable_caches( );
+    disable_caches ( );
 }
--- src/platform.h
+++ src/platform.h
@@ -35,7 +35,7 @@
 
 #include "platform_config.h"
 
-void init_platform( );
-void cleanup_platform( );
+void init_platform ( );
+void cleanup_platform ( );
 
 #endif
